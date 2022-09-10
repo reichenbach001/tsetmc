c2520a6c80dfa037eaa9bbc2d8f4ded3bdbac7bc
mahdi <mahdisamani513@gmail.com>
2022-09-10 14:42:48 +0430

    config file added

---
+++ tset_module/config2.py
@@ -0,0 +1,7 @@
+constant_vars = {'tset_share_URLs_file': 'URLs.txt',
+                 'main_url': 'http://www.tsetmc.com/tsev2/data/MarketWatchInit.aspx?h=0&r=0',
+                 'selecting_length': 30,
+                 'crawled_data_dir': './data2/',
+                 'crawling_url_1': 'http://www.tsetmc.com/tsev2/data/InstTradeHistory.aspx?i=',
+                 'crawling_url_2': '&Top=999999&A=1'
+                 }
